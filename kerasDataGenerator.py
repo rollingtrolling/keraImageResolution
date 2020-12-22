aa1c0ddf50168204c6aa4ba0b170a36bd4b5d75f
rollingtrolling <56955162+rollingtrolling@users.noreply.github.com>
2020-12-22 21:43:59 +0900

    Update kerasDataGenerator.py

--- kerasDataGenerator.py
+++ kerasDataGenerator.py
@@ -2,59 +2,51 @@ from keras.utils import Sequence
 import numpy as np
 from PIL import Image as pil
 
-# batch만큼 데이터를 새로 메모리에 올리고 이전거는 내려버림
-# 각 batch 요청때마다 getitem이 호출 되고 그 내부에서 data_generation으로 실제 데이터 읽음
-# 각 epoch 끝날때마다 on_epoch_end가 호출됨, 그냥 인덱스를 섞어서 랜덤하게 batch를 뽑게함
+
 class DataGenerator(Sequence):
-    def __init__(self, trainID, targetID, trainSize, targetSize, batch_size, shuffle=True):
-        self.trainID = trainID
-        self.targetID = targetID
-        self.trainSize = trainSize
-        self.targetSize = targetSize
+    def __init__(self, ID, batch_size, imgSize, shuffle=True):
+        self.ID = ID
+        self.imgSize = imgSize
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.on_epoch_end()
 
+
     # 각 epoch 마지막에 인덱스를 섞어줌
     def on_epoch_end(self):
-        self.indexes = np.arange(len(self.trainID))
+        self.indexes = np.arange(len(self.ID))
 
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def __data_generation(self, list_IDs_temp):
-        x = np.empty((self.batch_size, *self.trainSize, 3))
-        y = np.empty((self.batch_size, *self.targetSize, 3))
+    # train 이미지는 200x200으로 변환 후 리턴, target 이미지는 200x200 그대로 리턴
+    def __data_generation__(self, list_IDs_temp):
+        x = np.empty((self.batch_size, self.imgSize, self.imgSize, 3))
+        y = np.empty((self.batch_size, self.imgSize, self.imgSize, 3))
 
         for i, ID in enumerate(list_IDs_temp):
-            tmp = pil.open("E:/sourceImage/splitPictureResize/"+ID)
-            x[i,] = np.asarray(tmp).reshape((self.trainSize, 3))
+            train = pil.open("E:/sourceImage/splitPictureResize/" + ID) #***
+            train = train.resize((self.imgSize, self.imgSize))
+            x[i,] = np.asarray(train).reshape((self.imgSize, self.imgSize, 3))
 
-            tmp = pil.open("E:/sourceImage/splitPicture/"+ID)
-            y[i,] = np.asarray(tmp).reshape((self.targetSize, 3))
+            target = pil.open("E:/sourceImage/splitPicture/"+ID)
+            y[i,] = np.asarray(target).reshape((self.imgSize, self.imgSize, 3))
 
-        return x, y
+        return x/255, y/255
 
+
+    # 한 epoch에 존재하는 batch 수 return
     def __len__(self):
-        return int(np.floor(len(self.trainID) / self.batch_size))
+        return int(np.floor(len(self.ID) / self.batch_size))
+
 
     def __getitem__(self, index):
         # index에 맞는 데이터 이름을 batch만큼 뽑아서 data_generation으로 전달
-        # 100x100 이나 200x200 이나 이름은 같으니까 data_generation에 같은 이름 보내도 됨
+        # 100x100과 200x200이 서로 이름은 같으므로 data_generation에 같은 이름 보내도 됨
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
-        list_IDs_temp = [self.trainID[k] for k in indexes]
+        list_IDs_temp = [self.ID[k] for k in indexes]
 
-        x, y = self.__data_generation(list_IDs_temp)
+        x, y = self.__data_generation__(list_IDs_temp)
 
         return x, y
-
-"""
-# 이렇게 파일 리스트를 뽑아 위 클래스의 ID로 사용한다
-# 어차피 리사이즈 된 이미지나 원본이미지나 이름은 다 같은걸로 맞춰놨다
-# 이름만 뽑아서 클래스로 던져주는거니까 어디서 뽑든 상관 없다 
-dir = "E:/sourceImage/splitPictureResize/"
-filelist = os.listdir(dir)
-
-print(filelist[:10])
-"""
